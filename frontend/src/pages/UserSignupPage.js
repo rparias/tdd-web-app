8fbab2133b025b1519b788a24a2448e171e1de44
Ronald Arias <ronald.arias@thoughtworks.com>
2020-11-03 14:11:01 -0500

    [frontend] Adds tests for signup page layout

--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -4,6 +4,13 @@ const UserSignupPage = () => {
     return (
         <div>
             <h1>Sign Up</h1>
+            <div>
+                <input type="text" placeholder="Your display name"/>
+                <input type="text" placeholder="Your username"/>
+                <input type="password" placeholder="Your password"/>
+                <input type="password" placeholder="Repeat your password"/>
+                <button>Sign Up</button>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/UserSignupPage.spec.js
+++ frontend/src/pages/UserSignupPage.spec.js
@@ -11,6 +11,48 @@ describe('UserSignupPage', () => {
             const {container} = render(<UserSignupPage />);
             const header = container.querySelector('h1');
             expect(header).toHaveTextContent('Sign Up');
-        })
+        });
+
+        it('has input for display name', () => {
+            const {queryByPlaceholderText} = render(<UserSignupPage />);
+            const displayNameInput = queryByPlaceholderText('Your display name');
+            expect(displayNameInput).toBeInTheDocument();
+        });
+
+        it('has input for username', () => {
+            const {queryByPlaceholderText} = render(<UserSignupPage />);
+            const usernameInput = queryByPlaceholderText('Your username');
+            expect(usernameInput).toBeInTheDocument();
+        });
+
+        it('has input for password', () => {
+            const {queryByPlaceholderText} = render(<UserSignupPage />);
+            const passwordInput = queryByPlaceholderText('Your password');
+            expect(passwordInput).toBeInTheDocument();
+        });
+
+        it('has password type for password input', () => {
+            const {queryByPlaceholderText} = render(<UserSignupPage />);
+            const passwordInput = queryByPlaceholderText('Your password');
+            expect(passwordInput.type).toBe('password');
+        });
+
+        it('has input for password repeat', () => {
+            const {queryByPlaceholderText} = render(<UserSignupPage />);
+            const passwordRepeatInput = queryByPlaceholderText('Repeat your password');
+            expect(passwordRepeatInput).toBeInTheDocument();
+        });
+
+        it('has password type for password repeat input', () => {
+            const {queryByPlaceholderText} = render(<UserSignupPage />);
+            const passwordRepeatInput = queryByPlaceholderText('Repeat your password');
+            expect(passwordRepeatInput.type).toBe('password');
+        });
+
+        it('has submit button', () => {
+            const {container} = render(<UserSignupPage />)
+            const button = container.querySelector('button');
+            expect(button).toBeInTheDocument();
+        });
     })
 })
