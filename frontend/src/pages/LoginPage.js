bb6fae47b4d6d92d5ffae499b1b542771f2b43be
Ronald Arias <ronald.arias@thoughtworks.com>
2021-02-19 15:40:10 -0500

    [frontend] Handles input change on login page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../components/Input';
 
 const LoginPage = () => {
+  const [state, setState] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
   return (
     <div className="container">
       <h1 className="text-center">Login</h1>
       <div className="col-12 mb-3">
-        <Input label="Username" placeholder="Your username" name="username" />
+        <Input
+          label="Username"
+          placeholder="Your username"
+          name="username"
+          value={state.username}
+          onChange={handleOnChange}
+        />
       </div>
       <div className="col-12 mb-3">
         <Input
@@ -14,6 +32,8 @@ const LoginPage = () => {
           placeholder="Your password"
           name="password"
           type="password"
+          value={state.password}
+          onChange={handleOnChange}
         />
       </div>
       <div className="text-center">
